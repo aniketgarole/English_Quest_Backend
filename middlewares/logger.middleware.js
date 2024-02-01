25f2832a68a7e620a59f7102ae0add8eb15aba72
Aniket Garole <aniketgarole11@gmail.com>
2024-02-01 20:22:38 +0530

    changed logger middleware

--- middlewares/logger.middleware.js
+++ middlewares/logger.middleware.js
@@ -1,13 +1,7 @@
 const morgan = require('morgan');
-const fs = require('fs');
-const path = require('path');
-
-const accessLogStream = fs.createWriteStream(path.join(__dirname, '../requests.log'), { flags: 'a' });
 
 const loggerMiddleware = () => {
-  return morgan('combined', { stream: accessLogStream });
+  return morgan('combined');
 };
-
-
 
 module.exports = loggerMiddleware;
